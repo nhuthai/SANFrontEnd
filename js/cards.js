77908e6190347a559625ee400b5b3c7a5ba4fdc3
Hai Huynh <Nhut.H.Huynh@student.fh-Kiel.de>
2017-12-26 12:54:57 +0100

    Update the feature of updating user information

--- js/cards.js
+++ js/cards.js
@@ -1,3 +1,18 @@
 function addCard(photo, title, body, link) {
-	
+	$('#content').append(
+		'<div class="card horizontal">' +
+			'<div class="card-image">' +
+				'<img src="' + photo + '">' + 
+			'</div>' + 
+			'<div class="card-stacked">' + 
+				'<div class="card-content">' + 
+					'<h3>' + title + '</h3>' + 
+					'<p>' + body + '</p>' + 
+				'</div>' + 
+				'<div class="card-action">' + 
+					'<a href="' + link + '">Link</a>' + 
+				'</div>' + 
+			'</div>' + 
+		'</div>'
+		);
 }
